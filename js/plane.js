de6a188762ed15e865bdb4005dbb0e62777f14e5
pdimanov <pdimanov@abv.bg>
2016-01-06 21:34:05 +0200

    Plane moves now

--- js/plane.js
+++ js/plane.js
@@ -1,3 +1,62 @@
 var plane = document.getElementById('plane');
-var centerOfScreen = screen.availWidth/2 - 50;
-plane.style.left = centerOfScreen + 'px';
+
+var planeLeft = screen.availWidth/2 - 50;
+plane.style.left = planeLeft + 'px';
+var planeBottom = 0;
+plane.style.bottom = planeBottom + 'px';
+
+var pace = 3;
+var direction = {
+	left: false,
+	up: false,
+	right: false,
+	down: false
+};
+
+function planeDirection(key, bool){
+	switch(key){
+		case 37:
+			direction.left = bool;
+			break;
+		case 38:
+			direction.up = bool;
+			break;
+		case 39:
+			direction.right = bool;
+			break;
+		case 40:
+			direction.down = bool;
+			break;
+		default:
+			break;
+	}
+}
+
+window.addEventListener('load', function(){
+
+	document.addEventListener('keydown', function(event){
+		planeDirection(event.keyCode, true);
+	}, false);
+
+	document.addEventListener('keyup', function(event){
+		planeDirection(event.keyCode, false);
+	}, false);
+
+	setInterval(function(){
+		if(direction.left){
+			planeLeft -= pace;
+		}
+		if(direction.up){
+			planeBottom += pace;
+		}
+		if(direction.right){
+			planeLeft += pace;
+		}
+		if(direction.down){
+			planeBottom -= pace;
+		}
+
+		plane.style.left = planeLeft + 'px';
+		plane.style.bottom = planeBottom + 'px';
+	}, 1000/60)
+}, false);
